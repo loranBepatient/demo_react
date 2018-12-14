dc8c026cf3e5062e35b68f8316dca35f0fec4d48
LoranM <laurentm@me.com>
2018-12-14 05:46:29 +0100

    modules

--- src/index.js
+++ src/index.js
@@ -207,3 +207,17 @@ console.log(square2(10));
 // };
 
 // cookEgg2("egsdf"); */
+
+/* // MODULES
+// import { Teacher } from "./teacher";
+
+// Default => import ... from './'
+// Named => import {...} from './'
+// with default set for the export
+// import Teacher from "./teacher";
+// import Teacher, { promote } from "./teacher";
+
+// const Snape = new Teacher("Snape", 2);
+
+// console.log(Snape.teach());
+// console.log(promote()); */
---
+++ src/teacher.js
@@ -0,0 +1,30 @@
+// import { Person } from "./person";
+
+// export class Teacher extends Person {
+//   constructor(name, degree = 1) {
+//     super(name);
+//     this.degree = degree;
+//   }
+//   teach() {
+//     return "Expellarium !!!";
+//   }
+// }
+// export default class Teacher extends Person {
+//   constructor(name, degree = 1) {
+//     super(name);
+//     this.degree = degree;
+//   }
+//   teach() {
+//     return "Expellarium !!!";
+//   }
+// }
+
+/* // private
+function promote() { 
+    return "level 2";
+} */
+
+// named expose
+// export function promote() {
+//   return "level 2";
+// }
