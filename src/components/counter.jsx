6491ed6c03982c21c5061a1ece7555940ba5c9f7
LoranM <laurentm@me.com>
2018-12-14 06:12:46 +0100

    React Fragment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,16 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {};
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      //   <div>
+      //     <h1>Hello World</h1>
+      //     <button>Increment</button>
+      //   </div>
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
   }
 }
 
