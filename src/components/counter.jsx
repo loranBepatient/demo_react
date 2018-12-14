d2782b0a9778800ccffc9122da307b5809db44d3
LoranM <laurentm@me.com>
2018-12-14 07:03:37 +0100

    reset counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,54 +5,28 @@ class Counter extends Component {
     count: 0
   };
 
-  //   constructor() {
-  //     super();
-  //     this.handleIncrement = this.handleIncrement.bind(this);
-  //   }
-
-  //   handleIncrement() {
-  //     console.log("increment clicked", this);
-  //   }
-
-  //   handleIncrement = () => {
-  //     console.log("increment clicked", this);
-  //     let { count } = this.state;
-  //     count++;
-  //     this.setState({ count });
-  //   };
-//   handleIncrement = nb => {
-//     console.log("increment clicked", nb);
-//     let { count } = this.state;
-//     count = count + nb;
-//     this.setState({ count });
-//   };
+  handleIncrement = () => {
+    console.log("increment clicked", this);
+    let { count } = this.state;
+    count++;
+    this.setState({ count });
+  };
 
   render() {
     const { count } = this.state;
 
     return (
-      //   <React.Fragment>
-      //     <span className={this.getBadgeClasses(count)}>
-      //       {this.formatCount()}
-      //     </span>
-      //     <button
-      //       onClick={this.handleIncrement}
-      //       className="btn btn-secondary btn-sm"
-      //     >
-      //       Increment
-      //     </button>
-      //   </React.Fragment>
-    //   <React.Fragment>
-    //     <span className={this.getBadgeClasses(count)}>
-    //       {this.formatCount()}
-    //     </span>
-    //     <button
-    //       onClick={() => this.handleIncrement(1)}
-    //       className="btn btn-secondary btn-sm"
-    //     >
-    //       Increment
-    //     </button>
-    //   </React.Fragment>
+      <React.Fragment>
+        <span className={this.getBadgeClasses(count)}>
+          {this.formatCount()}
+        </span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </React.Fragment>
     );
   }
 
